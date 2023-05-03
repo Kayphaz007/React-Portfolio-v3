a4630209e2e9ab3f99cefc4e5c8fc88f7c90951a
Kayphaz007 <kayphaz007@gmail.com>
2023-05-03 02:00:27 +0100

    removed 3d tech stacks

--- frontend/src/components/Tech.tsx
+++ frontend/src/components/Tech.tsx
@@ -19,11 +19,26 @@ const Tech = () => {
         My Tech Stacks.
       </h2>
       <div className="flex flex-row flex-wrap justify-center gap-10">
-        {skills.map((skill) => (
+        {/* please fix and look for an alternative to representing tech stacks other than with balls */}
+
+        {/* present it in a circular way with light animated around its border */}
+        {/* <div className = " w-10 h-10 border rounded-full">
+        <img className="" src = {urlFor(skill?.icon ?? css).url()}/> </div> */}
+        {/* {skills.map((skill) => (
           <div className="w-28 h-28" key={skill.name}>
-            <BallCanvas icon={urlFor(skill?.icon??css).url()} />
+            <BallCanvas icon={urlFor(skill?.icon ?? css).url()} />
             <p className="flex justify-center items-center">{skill.name}</p>
           </div>
+        ))} */}
+        {skills.map((skill) => (
+          <>
+            <div className="w-28 h-28 rounded full" key={skill.name}>
+              {/* <BallCanvas icon={urlFor(skill?.icon ?? css).url()} />
+               */}
+              <img className="" src={urlFor(skill?.icon ?? css).url()} />
+            <p className="flex justify-center items-center">{skill.name}</p>
+            </div>
+          </>
         ))}
       </div>
     </>
